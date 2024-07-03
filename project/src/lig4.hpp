85efd6d90e01887d36ec7409e6641cf02ad3d3ff
Bowie <marketing@letscode.com.br>
2024-07-03 11:43:20 -0300

    Implementação do lig4

---
+++ project/src/lig4.hpp
@@ -0,0 +1,15 @@
+#ifndef LIG4_HPP
+#define LIG4_HPP
+
+#include "tabuleiro.hpp"
+
+class lig4 : public tabuleiro {
+public:
+    lig4(int lin, int col);
+
+    void makeMove(int line, int column, char player);
+    bool checkVictory(char player);
+    void printBoard();
+};
+
+#endif // CONNECT4_HPP
