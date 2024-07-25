42e05bb9f7a5ad69122eba01a194cd2c221d21b4
Bowie <juliasouzamoura1@gmail.com>
2024-07-25 17:15:45 -0300

    Ataualização do reversi

--- project/src/games/reversi.cpp
+++ project/src/games/reversi.cpp
@@ -1,27 +1,3 @@
-//#include "../include/reversi.hpp"
-//
-//Reversi::Reversi(int line, int column) : Board(line, column) {
-//}
-//
-//void Reversi::makeMove(int line, int column, int player) {
-//    if (validateMove(line, column)) {
-//        _board[line][column].setValue(player);
-//    }
-//}
-//
-//bool Reversi::checkVictory(int player) {
-//    return false;
-//}
-
-// void Reversi::printBoard() {
-//     for (int i = 0; i < linesSize; ++i) {
-//         for (int j = 0; j < columnsSize; ++j) {
-//             std::cout << _board[i][j].getValue() << " ";
-//         }
-//         std::cout << std::endl;
-//     }
-// }
-
 #include "reversi.hpp"
 
 const int BOARD_SIZE = 9;
