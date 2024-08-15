b67a8841f80af5cd6a0aa2df8534243e26ffd23b
Vanessa <ncsvanessa@gmail.com>
2024-08-15 16:18:47 -0300

    separação em cpp jogo checkers
    separação em cpp jogo checkers

--- src/checkers.cpp
+++ src/checkers.cpp
@@ -1,16 +1,14 @@
 #include "../include/checkers.hpp"
 
-#include <iostream>
-#include <string>
-
-void printBoard(int board[8][8])
+void CheckersGame::printBoard()
 {
+    int board[8][8];
     int x, y;
-    char z=1;
-    for (x=0;x<8;x++){
+    char z = 1;
+    for (x = 0; x < 8; x++){
         std::cout << z << std::endl;
         z++;
-        for (y=0;y<8;y++){
+        for (y = 0; y < 8; y++){
             std::cout << "|";
             switch(board[x][y])
             {
@@ -36,56 +34,57 @@ void printBoard(int board[8][8])
     std::cout << std::endl;
 }
 
-void setDefaults(int board[8][8])
+void CheckersGame::setDefaults(int board[8][8])
 {
-    board[0][1]=1;board[0][3]=1;board[0][5]=1;board[0][7]=1;
-    board[1][0]=1;board[1][2]=1;board[1][4]=1;board[1][6]=1;
-    board[2][1]=1;board[2][3]=1;board[2][5]=1;board[2][7]=1;
-    board[5][0]=2;board[5][2]=2;board[5][4]=2;board[5][6]=2;
-    board[6][1]=2;board[6][3]=2;board[6][5]=2;board[6][7]=2;
-    board[7][0]=2;board[7][2]=2;board[7][4]=2;board[7][6]=2;
+    board[0][1] = 1; board[0][3] = 1; board[0][5] = 1; board[0][7] = 1;
+    board[1][0] = 1; board[1][2] = 1; board[1][4] = 1; board[1][6] = 1;
+    board[2][1] = 1; board[2][3] = 1; board[2][5] = 1; board[2][7] = 1;
+    board[5][0] = 2; board[5][2] = 2; board[5][4] = 2; board[5][6] = 2;
+    board[6][1] = 2; board[6][3] = 2; board[6][5] = 2; board[6][7] = 2;
+    board[7][0] = 2; board[7][2] = 2; board[7][4] = 2; board[7][6] = 2;
 }
 
-int Enemy(int piece)
+int CheckersGame::Enemy(int piece)
 {
-    if(piece==1 || piece==3)
+    if(piece == 1 || piece == 3)
     {
         return 2;
     }
-    if(piece==2 || piece==4)
+    if(piece == 2 || piece == 4)
     {
         return 1;
     }
     return 0;
 }
 
-void CheckIfBecameQueen(int x, int y, int board[8][8], int turn)
+void CheckersGame::CheckIfBecameQueen(int x, int y, int board[8][8], int turn)
 {
-    if(turn==1)
+    if(turn == 1)
     {
-        if(x==7)
+        if(x == 7)
         {
-            board[x][y]=3;
+            board[x][y] = 3;
         }
     }
-    if(turn==2)
+    if(turn == 2)
     {
-        if(x==0)
+        if(x == 0)
         {
-            board[x][y]=4;
+            board[x][y] = 4;
         }
     }
 }
 
-void readMove(int x[]) {
+void CheckersGame::readMove(int x[])
+{
     std::cout << "Digite linha e coluna da peça: " << std::endl;
     std::cin >> x[1] >> x[2];
     std::cout << "Digite linha e coluna da jogada: " << std::endl;
     std::cin >> x[3] >> x[4];
 }
 
-
-bool validMove(int board[8][8], int x[4]) {
+bool CheckersGame::validMove(int board[8][8], int x[4])
+{
     if (x[1] < 0 || x[1] >= 8 || x[2] < 0 || x[2] > 8){
         std::cout << "Jogada invalida 1" << std::endl;
         return false;
@@ -104,7 +103,8 @@ bool validMove(int board[8][8], int x[4]) {
     }
 }
 
-int nextChainValid(int board[8][8], int x[4]) {
+int CheckersGame::nextChainValid(int board[8][8], int x[4])
+{
     int currentPiece = board[x[1]][x[2]];
     int enemyPiece1 = Enemy(currentPiece);
     int enemyPiece2 = Enemy(currentPiece + 2);
@@ -120,11 +120,13 @@ int nextChainValid(int board[8][8], int x[4]) {
     }
 }
 
-bool simpleMove(int x[4], int turn) {
+bool CheckersGame::simpleMove(int x[4], int turn)
+{
     return (x[3] == x[1] + 1 || x[3] == x[1] - 1) && (x[4] == x[2] + 1 || x[4] == x[2] - 1);
 }
 
-bool captureMove(int board[9][8],int x[4], int turn) {
+bool CheckersGame::captureMove(int board[8][8], int x[4], int turn)
+{
     if (nextChainValid(board, x) == 1){
         return (x[3] == x[1] + 2 || x[3] == x[1] - 2) && (x[4] == x[2] + 2 || x[4] == x[2] - 2);
     }
@@ -133,7 +135,8 @@ bool captureMove(int board[9][8],int x[4], int turn) {
     }
 }
 
-void updateBoard(int board[8][8], int x[4], int turn, int peshka[2]) {
+void CheckersGame::updateBoard(int board[8][8], int x[4], int turn, int peshka[2])
+{
     board[x[1]][x[2]] = 0;
     board[x[3]][x[4]] = turn;
     int x2 = (x[1] + x[3]) / 2;
@@ -144,7 +147,8 @@ void updateBoard(int board[8][8], int x[4], int turn, int peshka[2]) {
     }
 }
 
-void getCo(int board[8][8], int turn, int peshka[2]) {
+void CheckersGame::getCo(int board[8][8], int turn, int peshka[2])
+{
     int x[4];
     bool moveValido = false;
     while (!moveValido) {
@@ -168,14 +172,14 @@ void getCo(int board[8][8], int turn, int peshka[2]) {
     }
 }
 
-int Winner(int x, int o)
+int CheckersGame::Winner(int x, int o)
 {
-    if(x==0)
+    if(x == 0)
     {
         std::cout << "O won!" << std::endl;
         return 1;
     }
-    else if(o==0)
+    else if(o == 0)
     {
         std::cout << "X won!" << std::endl;
         return 1;
@@ -185,19 +189,3 @@ int Winner(int x, int o)
         return 0;
     }
 }
-
-// int main() {
-//     int board[9][8]={0}, turn=1, peshka[2]={12,12};
-//     set_defaults(board);
-//     print_board(board);
-
-//     while (true) {
-//         get_co(board, turn, peshka);
-//         print_board(board);
-//         if (Winner(peshka[0], peshka[1])) {
-//             break;
-//         }
-//         turn = (turn == 1) ? 2 : 1;
-//     }
-
-// }
