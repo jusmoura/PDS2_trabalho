07214c646cd5eec1219373fade7e7e3c77d757df
Daniela Dourado <148513611+danidouradoar@users.noreply.github.com>
2024-08-16 16:59:25 -0300

    criação da classe Lig4

--- include/lig4.hpp
+++ include/lig4.hpp
@@ -4,12 +4,15 @@
 #include "board.hpp"
 
 class Lig4 : public Board {
+private:
+    int currentPlayer;
 public:
-    Lig4(int lin, int col);
-
-    void makeMove(int line, int column, int player);
-    bool checkVictory(int player);
-    // void printBoard();
+    Lig4();
+    bool checkVictory(int currentPlayer,int board[6][7]);
+    bool checkTie(int board[6][7]);
+    void makeMove(int currentPlayer,int board[6][7],int column);
+    void switchPlayer(int currentPlayer, int playerX,int playerO);
+    void play(int board[6][7]);
 };
 
 #endif //LIG4_HPP
